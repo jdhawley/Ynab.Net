2ccc9e53df508f341776aafd6cf78f118898d480
Jonathan <jdhawley@crimson.ua.edu>
2019-03-28 19:13:04 -0500

    Added empty console application and xUnit testing projects

---
+++ Ynab.Net.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ynab.Net.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Ynab.Net.Tests/YnabClientTests.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace Ynab.Net.Tests
+{
+    public class YnabClientTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
